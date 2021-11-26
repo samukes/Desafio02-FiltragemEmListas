d30d5b1bab11494820ba6fea7a42aeb4f0afe51a
samukes <darkdarumia@gmail.com>
2021-11-26 14:35:11 -0300

    First commit

---
+++ lib/desafio02_list_filter.ex
@@ -0,0 +1,18 @@
+defmodule Desafio02ListFilter do
+  @moduledoc """
+  Documentation for `Desafio02ListFilter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Desafio02ListFilter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/desafio02_list_filter_test.exs
@@ -0,0 +1,8 @@
+defmodule Desafio02ListFilterTest do
+  use ExUnit.Case
+  doctest Desafio02ListFilter
+
+  test "greets the world" do
+    assert Desafio02ListFilter.hello() == :world
+  end
+end
